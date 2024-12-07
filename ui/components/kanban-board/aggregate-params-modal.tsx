2773b262edb375f8185dc262ecf38dbe0298f3b7
Garv Makkar <garv.codes@gmail.com>
2024-12-08 00:43:42 +0530

    add aggregation model

--- ui/components/kanban-board/aggregate-params-modal.tsx
+++ ui/components/kanban-board/aggregate-params-modal.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Dialog,
   DialogContent,
@@ -6,12 +8,56 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Merge, MoveRight } from "lucide-react";
+import { useEffect, useState } from "react";
+import CopyAddress from "../ui/copy-address";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "../ui/button";
+
+interface Contributor {
+  address: string;
+  blobID: string;
+}
 
 const AggregateParamsModalWrapper = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
+  const sampleContributors: Contributor[] = [
+    {
+      address: "0x1234567890abcdef",
+      blobID: "0x1234567890abcdef",
+    },
+    {
+      address: "0x1234567890abcdef",
+      blobID: "0x1234567890abcdef",
+    },
+    {
+      address: "0x1234567890abcdef",
+      blobID: "0x1234567890abcdef",
+    },
+  ];
+
+  const [contributors, setContributors] = useState<Contributor[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setContributors(sampleContributors);
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
@@ -24,6 +70,26 @@ const AggregateParamsModalWrapper = ({
             Aggregate parameters for the selected models{" "}
           </DialogDescription>
         </DialogHeader>
+
+        <Table>
+          <TableCaption>Contributors on your model.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Address</TableHead>
+              <TableHead>BlobID</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {contributors.map((contributor, index) => (
+              <TableRow key={index}>
+                <TableCell><CopyAddress className="text-sm" address={contributor.address} /></TableCell>
+                <TableCell><CopyAddress className="text-sm font-mono" address={contributor.blobID} /></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+
+        <Button className="mt-4">Aggregate <Merge className="w-4 ml-1" /></Button>
       </DialogContent>
     </Dialog>
   );
